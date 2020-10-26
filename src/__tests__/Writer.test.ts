b3028ef3586088635daeab111f7cb03d28eef439
highway <highway.ita07@web.de>
2020-10-26 15:36:54 +0100

    clean up

--- src/__tests__/Writer.test.ts
+++ src/__tests__/Writer.test.ts
@@ -40,7 +40,7 @@ test('MISC', () => {
     // look for the red annotation!
     fac.createHighlightAnnotation(0, [50, 50, 80, 80], "Test123", "Max", { r: 100, g: 16, b: 0 })
     fac.getAnnotations().then((annots) => {
-        console.log(annots)
+        //console.log(annots)
     })
     let data2 = fac.write()
     save('output.pdf', data2)
