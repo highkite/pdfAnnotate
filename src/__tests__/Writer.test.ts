ba5573b61da102d144dc33254dc1874e38b75692
highway <highway.ita07@web.de>
2021-10-30 12:04:35 +0200

    comment (very) long running tests

--- src/__tests__/Writer.test.ts
+++ src/__tests__/Writer.test.ts
@@ -23,55 +23,55 @@ test('write testDocument2', () => {
     fac.write()
 })
 
-test('writeTestDocument3', () => {
-    let data = loadFromFile('./test_documents/test18.pdf')
-    let fac: AnnotationFactory = new AnnotationFactory(data, "123")
+//test('writeTestDocument3', () => {
+//    let data = loadFromFile('./test_documents/test18.pdf')
+//    let fac: AnnotationFactory = new AnnotationFactory(data, "123")
+//
+//    fac.createTextAnnotation(0, [50, 50, 80, 80], "Test123", "Max")
+//    fac.createHighlightAnnotation(0, [50, 50, 100, 100], "Test1234", "Max")
+//
+//    fac.write()
+//    let output_data = fac.write()
+//
+//    let fac2: AnnotationFactory = new AnnotationFactory(output_data, "123")
+//
+//    fac2.getAnnotations().then((annots) => {
+//        console.log(annots)
+//    })
+//})
 
-    fac.createTextAnnotation(0, [50, 50, 80, 80], "Test123", "Max")
-    fac.createHighlightAnnotation(0, [50, 50, 100, 100], "Test1234", "Max")
+//test('getAnnotations', () => {
+//    let data = new Uint8Array(testDocument)
+//    let fac: AnnotationFactory = new AnnotationFactory(data)
+//
+//    fac.deleteAnnotation('okular-{4e2e238d-7f4f-4b99-ac92-6def305d4796}').then((toDelete) => {
+//        fac.deleteAnnotation('okular-{551f1c1c-224d-4982-b5ce-3494483bf8d3}').then((toDelete) => {
+//            fac.save()
+//        })
+//    })
+//})
 
-    fac.write()
-    let output_data = fac.write()
-
-    let fac2: AnnotationFactory = new AnnotationFactory(output_data, "123")
-
-    fac2.getAnnotations().then((annots) => {
-        console.log(annots)
-    })
-})
-
-test('getAnnotations', () => {
-    let data = new Uint8Array(testDocument)
-    let fac: AnnotationFactory = new AnnotationFactory(data)
-
-    fac.deleteAnnotation('okular-{4e2e238d-7f4f-4b99-ac92-6def305d4796}').then((toDelete) => {
-        fac.deleteAnnotation('okular-{551f1c1c-224d-4982-b5ce-3494483bf8d3}').then((toDelete) => {
-            fac.save()
-        })
-    })
-})
-
-test('MISC', () => {
-    let data = loadFromFile('./test_documents/test11.pdf')
-    let fac: AnnotationFactory = new AnnotationFactory(data)
-    // look for the red annotation!
-    fac.createHighlightAnnotation(0, [50, 50, 80, 80], "Test123", "Max", { r: 100, g: 16, b: 0 })
-    fac.getAnnotations().then((annots) => {
-        //console.log(annots)
-    })
-    let data2 = fac.write()
-    save('output.pdf', data2)
-
-    //let data = loadFromFile('./test6.pdf')
-    //let fac: AnnotationFactory = new AnnotationFactory(data)
-    //fac.createTextAnnotation(0, [50, 50, 80, 80], "Test123", "Max")
-    //let data2 = fac.write()
-    //save('output.pdf', data2)
-
-    //let fac2: AnnotationFactory = new AnnotationFactory(data2)
-    //console.log("here")
-    //fac2.getAnnotations().then((annots) => {
-    //    console.log("######################################################")
-    //    expect(annots).toEqual("abc")
-    //})
-})
+//test('MISC', () => {
+//    let data = loadFromFile('./test_documents/test11.pdf')
+//    let fac: AnnotationFactory = new AnnotationFactory(data)
+//    // look for the red annotation!
+//    fac.createHighlightAnnotation(0, [50, 50, 80, 80], "Test123", "Max", { r: 100, g: 16, b: 0 })
+//    fac.getAnnotations().then((annots) => {
+//        //console.log(annots)
+//    })
+//    let data2 = fac.write()
+//    save('output.pdf', data2)
+//
+//    //let data = loadFromFile('./test6.pdf')
+//    //let fac: AnnotationFactory = new AnnotationFactory(data)
+//    //fac.createTextAnnotation(0, [50, 50, 80, 80], "Test123", "Max")
+//    //let data2 = fac.write()
+//    //save('output.pdf', data2)
+//
+//    //let fac2: AnnotationFactory = new AnnotationFactory(data2)
+//    //console.log("here")
+//    //fac2.getAnnotations().then((annots) => {
+//    //    console.log("######################################################")
+//    //    expect(annots).toEqual("abc")
+//    //})
+//})
