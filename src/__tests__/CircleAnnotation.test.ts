eda569eceab8a1cba87539ce369e8e86353ad31f
highway <highway.ita07@web.de>
2021-11-18 16:48:00 +0100

    added additional tests to circle

--- src/__tests__/CircleAnnotation.test.ts
+++ src/__tests__/CircleAnnotation.test.ts
@@ -153,3 +153,48 @@ test('CircleAnnotation_appearance_stream_color', () => {
 
     expect(CryptoUtil.MD5Hex(factory.write())).toBe("9b686e0ae27c3a45f71399b188bcd6da")
 })
+
+test('CircleAnnotation_appearance_stream_border_only', () => {
+    let data = new Uint8Array(loadFromFile("./test_documents/test.pdf"))
+    let factory = new AnnotationFactory(data)
+
+    let color2 = {r:1, g:1, b:0}
+
+    let val = {
+        page: 0,
+        rect: [50, 50, 80, 80],
+        contents: "Test123",
+        author: "John",
+        updateDate: new Date(2021, 1, 1),
+        creationDate: new Date(2021, 1, 1),
+        id: "test-id-123",
+        color: color2,
+    }
+    factory.createCircleAnnotation(val)
+
+    expect(CryptoUtil.MD5Hex(factory.write())).toBe("f69791a41f344a5dbad2059e6f558638")
+})
+
+test('CircleAnnotation_appearance_stream_opacity', () => {
+    let data = new Uint8Array(loadFromFile("./test_documents/test.pdf"))
+    let factory = new AnnotationFactory(data)
+
+    let color = {r:1, g:0, b:0}
+    let color2 = {r:1, g:1, b:0}
+
+    let val = {
+        page: 0,
+        rect: [50, 50, 80, 80],
+        contents: "Test123",
+        author: "John",
+        opacity: 0.5,
+        updateDate: new Date(2021, 1, 1),
+        creationDate: new Date(2021, 1, 1),
+        id: "test-id-123",
+        color: color2,
+        fill: color
+    }
+    factory.createCircleAnnotation(val)
+
+    expect(CryptoUtil.MD5Hex(factory.write())).toBe("e4370574046a301bc67b03d99f209885")
+})
