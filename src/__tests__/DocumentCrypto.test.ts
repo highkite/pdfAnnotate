f27969cfad93e8e8be7cb9cee2d32ee68721024d
highway <highway.ita07@web.de>
2022-01-15 21:37:08 +0100

    added test for fetching added annotation

--- src/__tests__/DocumentCrypto.test.ts
+++ src/__tests__/DocumentCrypto.test.ts
@@ -158,4 +158,38 @@ test('encryptionTest18_appearance_stream', () => {
     ta.createDefaultAppearanceStream()
 
     expect(CryptoUtil.MD5Hex(factory.write())).toBe("7d3c2f1d58babc3372a2c5a752a2f302")
+
+    factory = new AnnotationFactory(factory.write(), "123", "")
+    factory.getAnnotations().then(vals => {
+        let annotations = vals[0]
+
+        expect(annotations[0].object_id!.obj).toBe(7)
+        expect(annotations[0].object_id!.generation).toBe(0)
+        expect(annotations[0].type).toBe('/Highlight')
+        expect(annotations[0].page).toBe(0)
+        expect(annotations[0].rect).toEqual([69.697, 47.4148, 96.4646, 58.2598])
+        expect(annotations[0].border).toEqual([0, 0, 1])
+        expect(annotations[0].color).toEqual([1, 1, 0])
+        expect(annotations[0].updateDate).toBe("D:20190101153417")
+
+        expect(annotations[1].object_id!.obj).toBe(8)
+        expect(annotations[1].object_id!.generation).toBe(0)
+        expect(annotations[1].type).toBe('/Text')
+        expect(annotations[1].page).toBe(0)
+        expect(annotations[1].rect).toEqual([77.7777777778, 83.7931904161, 83.7777777778, 89.7856242119])
+        expect(annotations[1].border).toEqual([0, 0, 1])
+        expect(annotations[1].color).toEqual([1, 1, 0])
+        expect(annotations[1].updateDate).toBe("D:20190101154225")
+        expect(annotations[1].contents).toBe("Pop up note")
+        expect((annotations[1] as MarkupAnnotationObj).author).toBe("highway")
+
+        expect(annotations[2].object_id!.obj).toBe(10)
+        expect(annotations[2].object_id!.generation).toBe(0)
+        expect(annotations[2].type).toBe('/Text')
+        expect(annotations[2].page).toBe(0)
+        expect(annotations[2].rect).toEqual([50, 50, 70, 80])
+        expect(annotations[2].border).toEqual([0, 0, 1])
+        expect(annotations[2].color).toEqual([1, 1, 0])
+        expect(annotations[2].updateDate).toBe("D:20210201000000")
+    })
 })
